0cdd8f6df623af703100b3498dfc22bfe8668d8c
SimoneOnesta <simone.onesta96@gmail.com>
2020-01-14 18:49:38 +0100

    complete stats in prometheus

--- apigateway/src/main/java/com/dsproject/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/dsproject/apigateway/ApigatewayApplication.java
@@ -2,6 +2,7 @@ package com.dsproject.apigateway;
 
 // import io.prometheus.client.springboot.EnableSpringBootMetricsCollector;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import org.apache.logging.log4j.util.StackLocator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -16,21 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApigatewayApplication {
-    //@Value(value = "${VMS_HOST}")
-    private String vms = "http://vms:8080";
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayApplication.class, args);
     }
+    @Value(value = "${VMS_HOST}")
+    private String vms;
+
+    vmsRequestFilter filter = new vmsRequestFilter();
+
     @Autowired
-    LoggingGatewayFilterFactory filter;
+    MetricsController metrics;
 
     @Bean
     public RouteLocator  Gateway(RouteLocatorBuilder builder){
 
+
         return builder.routes().route(
                 p -> p.path("/vms/**")
-                        .filters(f-> f.rewritePath("/vms/(?<service>.*)","/${service}").)
+                        .filters(f-> f.rewritePath("/vms/(?<service>.*)","/${service}")
+                        .filter(filter.apply(new vmsRequestFilter.Config(metrics))))
                         .uri(vms)
         ).route(
                 p ->p.path("/videofiles/**")
@@ -41,6 +49,3 @@ public class ApigatewayApplication {
 
 
 }
-
-
-//.filters(f-> f.rewritePath("/vms/users/register(?<service>.*)","/${service}"))
--- apigateway/src/main/java/com/dsproject/apigateway/MetricsController.java
+++ apigateway/src/main/java/com/dsproject/apigateway/MetricsController.java
@@ -1,35 +1,49 @@
 package com.dsproject.apigateway;
 
+
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
+
+
 
 
 @Component
 public class MetricsController {
 
-    private final Counter myCounter;
-    private final Timer myTimer;
+
+
+    public final Timer users_register_timer;
+    public final Timer users_get_timer;
+    public final Timer videos_post;
+    public final Timer videos_post_id;
+    public final Timer videos_get;
+    public final Timer videos_get_id;
+    public final Counter request_counter;
+    public final Timer videofiles_get_id;
+    public final DistributionSummary requests_size;
+    public final DistributionSummary response_size;
+    public final Counter error_counter;
 
     public MetricsController(PrometheusMeterRegistry meterRegistry) {
-        myCounter = Counter
-                .builder("pippo")
-                .description("vms Get Couter")
-                .register(meterRegistry);
-        myTimer = Timer.builder("MyTimer").register(meterRegistry);
+
+        request_counter = Counter.builder("request_counter").register(meterRegistry);
+        users_register_timer = Timer.builder("users_register_timer").tag("method","POST").tag("URI","http://vms:8080/users/register").register(meterRegistry);
+        users_get_timer = Timer.builder("user_get_timer").tag("method","GET").tag("URI","http://vms:8080/users/").register(meterRegistry);
+        videos_post = Timer.builder("videos_post").tag("method","POST").tag("URI","http://vms:8080/videos/").register(meterRegistry);
+        videos_post_id = Timer.builder("videos_post_id").tag("method","POST").tag("URI","http://vms:8080/videos/{id}").register(meterRegistry);
+        videos_get = Timer.builder("videos_get").tag("method","GET").tag("URI","http://vms:8080/videos/").register(meterRegistry);
+        videos_get_id = Timer.builder("videos_get_id").tag("method","GET").tag("URI","http://vms:8080/video/{id}").register(meterRegistry);
+        videofiles_get_id = Timer.builder("videofiles_get_id").tag("method","GET").tag("URI","http://videofiles/{id}/video.mpd").register(meterRegistry);
+        requests_size = DistributionSummary.builder("request.size").register(meterRegistry);
+        response_size = DistributionSummary.builder("response.size").register(meterRegistry);
+        error_counter = Counter.builder("error_counter").register(meterRegistry);
+
     }
-
-    public void increment(){
-        myCounter.increment();
-
-    }
-
 }
 
 
